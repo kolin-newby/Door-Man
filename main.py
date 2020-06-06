7a1c65cd86b836ea131868d6953360a9dbd8b8cc
kolin-newby <kolin.newby@gmail.com>
2020-06-06 17:37:27 -0600

    initial push

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import cv2
+import numpy as np
+
+root = tk.Tk()
+# creat label widget
+myLabel = tk.Label(root, text = "Hello World!")
+# pack it onto screen
+myLabel.pack()
+
+
+root.mainloop()
+
