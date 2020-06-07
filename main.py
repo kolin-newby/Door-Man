2f7e72d9e65f9ce031e7cf3ea9cf3153e211f2d8
Kolin Newby <kolin.newby@gmail.com>
2020-06-06 18:30:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ root = tk.Tk()
 # creat label widget
 myLabel = tk.Label(root, text = "The Door Man!")
 # pack it onto screen
-myLabel.place(0.5, 0.5)
+myLabel.place(relheight = 0.5, relwidth = 0.5)
 
 
 root.mainloop()
