e1093b8b6e3be574ff824ab6926cd456a0be0026
Kolin Newby <kolin.newby@gmail.com>
2020-06-06 20:05:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import numpy as np
 
 win = Tk()
 win.attributes('-fullscreen', True)
-win.title('Door-Man')
+win.title('Door-Man App')
 # creat label widget
 cam_start_btn = Button(win, text = "Door Cam", height = 5, width = 20)
 # pack it onto screen
 cam_start_btn.pack(side=BOTTOM, expand=YES)
 
+feed = Frame(win, height = 50, width = 50)
+feed.pack(side = TOP, expand = YES)
 
 win.mainloop()
 
