1345da8afc3d8c19c63c341f9f7fe6a5c4b7dc65
Kolin Newby <kolin.newby@gmail.com>
2020-06-06 18:31:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import tkinter as tk
 import cv2
 import numpy as np
 
-root = tk.Tk()
+win = tk.Tk()
 # creat label widget
-myLabel = tk.Label(root, text = "The Door Man!")
+myLabel = tk.Label(win, text = "The Door Man!")
 # pack it onto screen
 myLabel.place(relheight = 0.5, relwidth = 0.5)
 
 
-root.mainloop()
+win.mainloop()
 
