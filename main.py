32a215a31fffa21e9ffde9456e26264df7512ce3
Kolin Newby <kolin.newby@gmail.com>
2020-06-06 23:20:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,34 @@ from tkinter import *
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+win = Tk()
+win.attributes('-fullscreen', True)
+win.title('Door-Man App')
 
-while(cap.isOpened()):
-	ret, frame = cap.read()
+cam_toggle_btn = Button(win, text = 'Door Cam', height = 5, width = 20)
+cam_toggle_btn.pack(side = BOTTOM, expand = YES)
 
-	cv2.imshow('output', frame)
-	if(cv2.waitKey(1) & 0xFF == ord('q')):
-		break
+win.mainloop()
 
-cap.release()
-cv2.destroyAllWindows()
+def doorFeed():
+	cap = cv2.VideoCapture(0)
+	while(cap.isOpened()):
+		ret, frame = cap.read()
+
+		cv2.imshow('output', frame)
+		if(cv2.waitKey(1) & 0xFF == ord('q')):
+			break
+
+	cap.release()
+	cv2.destroyAllWindows()
+
+def close():
+	win.destroy()
+
+
+
+feed = Frame(win)
+
+
+win.protocol("WM_DELETE_WINDOW", close)
+
