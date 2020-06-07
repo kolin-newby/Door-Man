c173657d67ce38a58fbc456e88ab7d0fe43774b4
Kolin Newby <kolin.newby@gmail.com>
2020-06-06 19:43:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ win = tk.Tk()
 win.attributes('-fullscreen', True)
 win.title('Door-Man')
 # creat label widget
-cam_start_btn = tk.Button(win, text = "Door Cam")
+cam_start_btn = tk.Button(win, text = "Door Cam", height = 20, width = 40)
 # pack it onto screen
 cam_start_btn.place(relx = 0.5, rely = 0.5)
 
