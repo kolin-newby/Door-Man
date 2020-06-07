e484ade14eaf651e7c693e75f7ea8eb8d7eace0e
Kolin Newby <kolin.newby@gmail.com>
2020-06-06 18:37:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ import numpy as np
 
 win = tk.Tk()
 # creat label widget
-myLabel = tk.Label(win, text = "The Door Man!")
+myLabel = tk.Label(win, text = "The Door-Man Cam!")
 # pack it onto screen
 myLabel.place(relx = 0.5, rely = 0.5)
 
 
 win.mainloop()
 
+
+#Functions
+def close():
+	win.destroy()
+
+
+win.protocol("WM_DELETE_WINDOW", close)
