ac16a8fa45144554e563b7dd682a9c1fea9ffb3b
Kolin Newby <kolin.newby@gmail.com>
2020-06-06 19:49:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ win.title('Door-Man')
 # creat label widget
 cam_start_btn = tk.Button(win, text = "Door Cam", height = 20, width = 40)
 # pack it onto screen
-cam_start_btn.grid(row = 1, column = 0)
+cam_start_btn.pack(side = BOTTOM, expand = YES)
 
 
 win.mainloop()
