e548b80e6977d74cba7cb0f6d74df3f51686d4d8
Kolin Newby <kolin.newby@gmail.com>
2020-06-06 22:31:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
 from tkinter import *
 import cv2
-from PIL import Image
-# from PIL import ImageTk
 import numpy as np
 
-class doorFeed:
-	panel = None
-	win = None
-	cam = None
+cap = cv2.VideoCapture(0)
 
-	def __init__(self):
-		self.win = Tk()
-		self.win.title('Door-Man')
-		self.win.attributes('-fullscreen', True)
+while(cap.isOpened()):
+	ret, frame = cap.read()
 
-		self.panel = Label(self.win)
-		self.panel.pack(side = BOTTOM, expand = YES)
+	cv2.imshow('output', frame)
+	if(cv2.waitKey(1) & 0xFF == ord('q')):
+		break
 
-		self.cam = cv2.VideoCapture(0)
-		self.camera1()
-		self.win.mainloop()
-
-	def camera1(self):
-		_,frame = self.camera.read()
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-		frame = Image.fromarray(frame)
-		frame = ImageTk.PhotoImage(frame)
-		self.panel.configure(image = frame)
-		self.panel.image = frame
-		self.panel.After(1, self.Camera)
-
-
-if __name__ == '__main__':
-	objVideo = doorFeed()
+cap.release()
+cv2.destroyAllWindows()
